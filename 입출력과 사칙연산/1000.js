d1b6c97fcc1905343020f3c5ca4317edaff3d4f7
hyeran0513 <hr_0513@naver.com>
2022-05-09 07:55:26 +0900

    조건문 commit

---
+++ 입출력과 사칙연산/1000.js
@@ -0,0 +1,17 @@
+/*
+let inputData = require("fs")
+  .readFileSync("example.txt")
+  .toString()
+  .split(" ");
+*/
+
+const fs = require("fs");
+const inputData = fs
+  .readFileSync(0, "utf8")
+  .toString()
+  .split(" ");
+
+const A = parseInt(inputData[0]);
+const B = parseInt(inputData[1]);
+
+console.log(A + B);
---
+++ 입출력과 사칙연산/README.md
@@ -0,0 +1,4 @@
+> 자바 스크립트 입출력 기본
+
+const fs = require("fs");
+const inputData = fs.readFileSync(0, "utf8").toString().split(" ");
